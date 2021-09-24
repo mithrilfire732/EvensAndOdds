839971c28a561ccdfa297ac0f6e4a52e3978094d
Alexander Kidd <alexander.t.kidd@gmail.com>
2021-09-24 13:47:23 -0400

    Add project files.

---
+++ EvensAndOdds/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EvensAndOdds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter an integer: ");
+            var answer = Console.ReadLine();
+            var number = Convert.ToInt32(answer);
+            if (number % 2 == 0)
+            {
+                Console.WriteLine($"The number {number} is even");
+            }
+            else if (number % 2 != 0)
+            {
+                Console.WriteLine($"The number {number} is odd");
+            }
+        }
+    }
+}
